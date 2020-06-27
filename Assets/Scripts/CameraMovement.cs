331b16d00a96004aa86cca04fa04bde5d6864b57
emonzon42 <elijah.monzon42@gmail.com>
2020-06-27 13:23:10 -0400

    initial

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public float cameraDistance = 30.0f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+            transform.position = new Vector3(player.position.x, player.position.y+4, transform.position.z);
+  
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+
+    public enum type {platformer, topDown }
+
+    public float moveSpeed, jumpHeight;
+    public bool onGround = false;
+    public Joystick joystick;
+    public float horizontalMove;
+    public float jump;
+
+    // Update is called once per frame
+    void Update(){
+
+        horizontalMove = transform.position.x; //joystick.Horizonntal
+        if (horizontalMove > 0)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+        }
+        else if (horizontalMove < 0)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+        }
+        jump = transform.position.y;//joystick.Vertical;
+        //td: change jump to be a tap on the right side of the screen
+        //td: attack is a double tap on right screen (or implement two buttons one for jump one for attack 
+
+        
+
+        if(jump > 0 && onGround)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+       if (obj.gameObject.tag == "ground" && jump == 0)
+        {
+            onGround = true;
+        } 
+    }
+
+    void OnCollisionExit2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "ground") 
+        {
+            onGround = false;
+        }
+    }
+
+}
