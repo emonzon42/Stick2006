70fbe4b6112ecc458c1b100e8d217ab60ac88196
emonzon42 <elijah.monzon42@gmail.com>
2020-06-27 23:27:23 -0400

    added platform generator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-
 {
-
-    public enum type {platformer, topDown }
+    //players movement
 
     public float moveSpeed, jumpHeight;
     public bool onGround = false;
@@ -17,7 +15,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update(){
 
-        horizontalMove = transform.position.x; //joystick.Horizonntal
+        horizontalMove = joystick.Horizontal;
+
+        //todo: remove joystick and manual x-axis movement (limit to jumping)
+
         if (horizontalMove > 0)
         {
             transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
@@ -26,13 +27,11 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
         }
-        jump = transform.position.y;//joystick.Vertical;
+
         //td: change jump to be a tap on the right side of the screen
         //td: attack is a double tap on right screen (or implement two buttons one for jump one for attack 
 
-        
-
-        if(jump > 0 && onGround)
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
         }
@@ -40,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D obj)
     {
-       if (obj.gameObject.tag == "ground" && jump == 0)
+       if (obj.gameObject.tag == "ground")
         {
             onGround = true;
         } 
