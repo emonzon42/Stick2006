e02547dfc7e89d2e6d5fa452de3a44ed6c241b16
emonzon42 <elijah.monzon42@gmail.com>
2020-06-30 14:31:58 -0400

    Added script to destroy objects that are no longer in players fov

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    //destroys gameobjects that are far from the player
+
+    private const float SPAWN_DISTANCE = 200f; //the distance between player and next object (de)spawn
+    [SerializeField] public GameObject player;
+
+    void Awake()
+	{
+        player = GameObject.Find("Player");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+ //       if(gameObject.GetComponent<ObjectDestroyer>() != null)
+        if (transform.position.x < player.transform.position.x - SPAWN_DISTANCE) //if object is further than set spawn distance from player (should be behind player)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformGenerator : MonoBehaviour
+{
+    //how the platforms generate
+
+    private const float SPAWN_DISTANCE = 200f; //the furthest distance between player and next platform spawn
+
+    [SerializeField] public GameObject player;
+    [SerializeField] private Transform levelStart; //start of level
+    [SerializeField] private List<Transform> platformParts; //all platform parts that can be spawned
+
+    private Vector3 lastEndPos; //last end position of platform
+
+
+    private void Awake()
+    {
+        lastEndPos = levelStart.Find("EndPos").position;
+        
+        int startingSpawns = 5;
+        for(int i = 0; i < startingSpawns; i++){
+            SpawnPlatform();
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, lastEndPos) < SPAWN_DISTANCE)
+            SpawnPlatform();
+    }
+
+    private void SpawnPlatform()
+    {
+        Transform chosenPart = platformParts[Random.Range(0, platformParts.Count)];
+
+        Transform lastPlatform = SpawnPlatform(chosenPart, lastEndPos);
+
+        lastEndPos = lastPlatform.Find("EndPos").position;
+
+    }
+
+    private Transform SpawnPlatform(Transform platform, Vector3 spawnPos)
+    {
+        Transform lastPlatform = Instantiate(platform, spawnPos, Quaternion.identity);
+        
+        return lastPlatform;
+    }
+
+}
