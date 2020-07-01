e5ed34c69b7c162d4ae1188469f420ca9d5f770e
emonzon42 <elijah.monzon42@gmail.com>
2020-07-01 14:44:42 -0400

    set up foundation for death mechanic

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class to hold all player data during game
+public class PlayerData : MonoBehaviour
+{
+
+    public  bool isDead;
+    public int score;
+    public int numOfCoins;
+    private Vector3 lastGroundPos;
+    private bool onGround = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        score = 0;
+        numOfCoins = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (onGround)
+            lastGroundPos = transform.position;
+
+        if (transform.position.y < lastGroundPos.y - 10f)
+            isDead = true;
+
+        if (isDead)
+            Application.isPlaying = false; //todo: change to die screen
+        else
+            score++;
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    { 
+        if (obj.gameObject.tag == "ground")
+        {
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "ground")
+        {
+            onGround = false;
+        }
+    }
+}
