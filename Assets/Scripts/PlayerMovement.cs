e63b670f020e45a6631630f4f313de6ea1d85479
emonzon42 <elijah.monzon42@gmail.com>
2020-07-01 19:55:28 -0400

    Player speed now increases over time

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,28 +9,14 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed, jumpHeight;
     public bool onGround = false;
     public Joystick joystick; //dev control (wont be in final release)
-    public float horizontalMove;
     public float jump = 0;
 
     // Update is called once per frame
     void Update(){
+        if (Time.fixedTime % 10 == 0) //every 10 seconds
+            moveSpeed++;
 
-        //horizontalMove = joystick.Horizontal; //if (horizontalMove > 0)
-
-        //todo: remove joystick and manual x-axis movement (limit to jumping)
         transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
-
-       /* if (horizontalMove > 0)
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
-        }
-        else if (horizontalMove < 0)
-        {
-            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
-        }*/
-
-        //td: change jump to be a tap on the right side of the screen
-        //td: attack is a double tap on right screen (or implement two buttons one for jump one for attack 
 
         if(Input.GetKeyDown(KeyCode.Space) && onGround)
         {
