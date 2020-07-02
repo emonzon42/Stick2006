ccdd14be28e2629a7bda1e5e7f8871b64cbf7629
emonzon42 <elijah.monzon42@gmail.com>
2020-07-02 18:45:22 -0400

    Added movement data to PlayerData script / implemented double jump

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//class to hold all player data during game
+//class to hold all player data during game (Movement/Score/Life)
 public class PlayerData : MonoBehaviour
 {
+    public float moveSpeed, jumpHeight; //movement
+    public bool onGround, firstLaunch; // ..
 
-    public bool dead;
-    public int score;
-    public int numOfCoins;
-    private Vector3 lastGroundPos;
-    private bool onGround = false;
+    public bool dead; //Life
+
+    public int score, numOfCoins; //Score
+
+    private Vector3 lastGroundPos; //last held position on ground block
 
     // Start is called before the first frame update
     void Start()
@@ -23,16 +25,46 @@ public class PlayerData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (onGround)
+		if (Alive() )
+		{
+            Movement();
+		}
+		else
+		{
+            ;//todo: change to die screen
+        }
+    }
+
+    //checks every frame update if player is alive and whether to continue giving points points
+    bool Alive()
+	{
+        if (onGround)
             lastGroundPos = transform.position;
 
-        if (transform.position.y < lastGroundPos.y - 20f)
+        if (transform.position.y < lastGroundPos.y - 20f) //if player drops 
             dead = true;
 
         if (dead)
-            ; //todo: change to die screen
+            return false; 
         else if (transform.position.x > lastGroundPos.x)
             score += 10;
+        return true;
+    }
+
+    void Movement()
+	{
+        if (Time.fixedTime % 10 == 0) //every 10 seconds
+            moveSpeed++;
+
+        transform.Translate(Vector2.right * Time.deltaTime * moveSpeed); //player auto run
+
+        if (Input.GetKeyDown(KeyCode.Space) && (onGround || firstLaunch)) //player can only double jump before hitting ground
+        {
+            if (firstLaunch)
+                firstLaunch = false; //prevent triple/etc jumping
+
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse); //player jump
+        }
     }
 
     void OnCollisionEnter2D(Collision2D obj)
@@ -40,7 +72,9 @@ public class PlayerData : MonoBehaviour
         if (obj.gameObject.tag == "ground")
         {
             onGround = true;
-        } else if (obj.gameObject.tag == "enemy")
+            firstLaunch = false;
+        }
+        else if (obj.gameObject.tag == "enemy")
 		{
             dead = true;
 		}
@@ -51,6 +85,7 @@ public class PlayerData : MonoBehaviour
         if (obj.gameObject.tag == "ground")
         {
             onGround = false;
+            firstLaunch = true;
         }
     }
 }
