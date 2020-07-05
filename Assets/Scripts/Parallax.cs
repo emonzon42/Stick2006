009971d487057d039c333b1274f1abdf0f87de5c
emonzon42 <elijah.monzon42@gmail.com>
2020-07-05 14:22:19 -0400

    added background for scene

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameObject cam;
+    public float parallaxIntensity;
+    private float length, startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1-parallaxIntensity)); //how far moved relative to camera
+        float dist = (cam.transform.position.x * parallaxIntensity);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y);
+
+        //ensures background never ends as movement happens
+        if (temp > startPos + length)
+            startPos += length;
+        else if (temp < startPos - length)
+            startPos -= length;
+    }
+}
