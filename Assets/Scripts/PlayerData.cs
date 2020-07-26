d15d2e675e4d80fea704fc929b72473c92498b28
emonzon42 <elijah.monzon42@gmail.com>
2020-07-26 19:02:08 -0400

    added jumping & landing animations

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -23,11 +23,12 @@ public class PlayerData : MonoBehaviour
         score = 0;
         numOfCoins = 0;
         startPos = transform.position;
+        
     }
 
     void FixedUpdate()
     {
-        if (!GetComponent<SpriteRenderer>().IsVisibleFrom(Camera.main)) //if player is no longer in view of camera (i.e. player crashed too many times)
+        if (!GetComponent<Renderer>().IsVisibleFrom(Camera.main)) //if player is no longer in view of camera (i.e. player crashed too many times)
             dead = true;
     }
 
@@ -56,14 +57,16 @@ public class PlayerData : MonoBehaviour
     //players movement data
     Vector3 Movement()
 	{
-
-        transform.Translate(Vector2.right * Time.deltaTime * moveSpeed); //player auto run           
+        transform.Translate(Vector2.right * Time.deltaTime * moveSpeed); //player auto run
 
         bool jumpButtonPressed = (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)); //player tapped the screen / pressed space button
         bool onGround = (Physics2D.OverlapCircle(feetPos.position, footRadius, whatIsGround) && !isJumping);
 
         if (onGround)
+        {
             jumpCount = 0; //player jump recharges when hitting ground
+            GetComponent<Animator>().SetTrigger("Run"); //Run animation
+        }
 
         if (jumpButtonPressed && jumpCount < 2) //player can only double jump before hitting ground
         {
@@ -71,23 +74,31 @@ public class PlayerData : MonoBehaviour
             isJumping = true;
             jumpTimeCounter = jumpTime;
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse); //player jump
+            GetComponent<Animator>().SetTrigger("Jump"); //jump animation
         }
 
         /* Length of time holding jump button affects how high in the air player goes */
         bool jumpButtonHeld = (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Space));
+
         if (jumpButtonHeld && isJumping)
         {
-            if(jumpTimeCounter > 0)
+            if (jumpTimeCounter > 0)
             {
                 GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse); //player jump
                 jumpTimeCounter -= Time.deltaTime;
             }
             else
+            {
                 isJumping = false;
+                GetComponent<Animator>().SetTrigger("Land"); //land animation
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) //lets go of jump button
+        if ((Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) && isJumping) //lets go of jump button
+        { 
             isJumping = false;
+            GetComponent<Animator>().SetTrigger("Land"); //land animation
+        }
 
         return transform.position;
     }
