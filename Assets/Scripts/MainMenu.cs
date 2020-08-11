ebea186a59f66aa6dd0226e2fd6db53d8bbc7e37
emonzon42 <elijah.monzon42@gmail.com>
2020-08-11 11:45:52 -0400

    highscore, last score, and total coins can be viewed in start menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public TextMeshProUGUI highScore, totalCoins, lastScore;
+
+    public void Start()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        highScore.text = "HI: " + data.highScore.ToString();
+        totalCoins.text = "COINS: " + data.totalCoins.ToString();
+        lastScore.text = "LAST: " + data.lastScore.ToString();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
